f16298b2582395a2a15d55d87482579b5c5f2e4d
= <=>
2023-12-21 13:23:48 +0530

    chore: change server name

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
 // export const SERVER_URL = 'http://localhost:5000'
-export const SERVER_URL = 'https://medi-assist-backend.vercel.app/'
+export const SERVER_URL = 'https://ai-interior-design-backend.vercel.app/'
